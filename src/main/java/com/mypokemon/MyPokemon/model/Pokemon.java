295902bdeb347c99bd4b418969d4b6bd5197d437
Gustavo <gustavo.postigo@mail.usf.edu.br>
2023-11-11 20:28:06 -0300

    iniciando projeto com banco de dados h2

---
+++ src/main/java/com/mypokemon/MyPokemon/model/Pokemon.java
@@ -0,0 +1,24 @@
+package com.mypokemon.MyPokemon.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
+public class Pokemon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String tipo;
+    private int nivel;
+    private String pokebola;
+
+}
